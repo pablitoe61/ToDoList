943b104696e903bf023516a5123747d1f19e4336
Pablo Espinoza <pablitoe61@gmail.com>
2025-02-10 21:52:24 -0600

    se corrigen estilos en responsive y se agregan estilos en general

--- src/ToDoCounter.js
+++ src/ToDoCounter.js
@@ -3,12 +3,14 @@ import "./ToDoCounter.css";
 function ToDoCounter({ total, completed }) {
   return (
     <>
+    <div className="ToDoCounter">
       <h1 className="ToDoCounter-Title" >
         Tus Tareas
       </h1>
       <h2 className="ToDoCounter-Subtitle">
         Has completado 8{completed} de 10{total} Tareas
       </h2>
+    </div>
     </>
   );
 }
